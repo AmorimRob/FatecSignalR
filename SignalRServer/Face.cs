84d5430524c13a95455becb00123cb78811d4d1c
Robson Amorim <robson.amorim@outlook.com>
2020-03-03 00:45:34 -0300

    initial commit

---
+++ SignalRServer/Face.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRServer
+{
+    public class Face
+    {
+        public string Emoção { get; set; }
+        public string Idade { get; set; }
+    }
+}
---
+++ SignalRServer/FaceHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRServer
+{
+    public class FaceHub : Hub
+    {
+        public async Task EnviarPedido(Face face) =>
+            await Clients.All.SendAsync("IdentificarFace", face);
+    }
+}
